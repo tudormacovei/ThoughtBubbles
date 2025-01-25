cce20a5074ecf0b90ab015adea268fbb47e1760b
TheDutchKnot <Joris.Plagge@gmail.com>
2025-01-25 22:22:25 +0100

    Development Handles

--- Assets/Scripts/DialogSystem/ChoiceSelect.cs
+++ Assets/Scripts/DialogSystem/ChoiceSelect.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 public class ChoiceSelect : MonoBehaviour
 {
+    //Cat dies after x choices made!
+    static int ChoicesMade = 0;
+
     public int Damage = 0;
 
     void Start()
@@ -12,6 +15,7 @@ public class ChoiceSelect : MonoBehaviour
 
     void Select()
     {
+        ChoicesMade++;
         Debug.Log(Damage);
         DialogManager.Instance.InactivateDialog();
     }
--- Assets/Scripts/DialogSystem/DialogData.cs
+++ Assets/Scripts/DialogSystem/DialogData.cs
@@ -2,6 +2,8 @@
 public class DialogData
 {
     public string question;
+
+    //Map each choice to a corresponding index of damage
     public string[] choices;
     public int[] damageNumber;
 }
