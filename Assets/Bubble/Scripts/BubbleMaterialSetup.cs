8b069ad60b5a325ae948863177d89c72b3a8026e
Macovei Tudor <tudor_macovei@iCloud.com>
2025-01-26 11:23:48 +0100

    Add randomized animation for bubbles

--- Assets/Bubble/Scripts/BubbleMaterialSetup.cs
+++ Assets/Bubble/Scripts/BubbleMaterialSetup.cs
@@ -1,19 +1,39 @@
+using System.Collections.Generic;
 using Unity.Hierarchy;
 using UnityEngine;
 
 public class BubbleMaterialSetup : MonoBehaviour
 {
-    public Renderer spriteRenderer;
-    public float val;
+    public List<float> frameCount;
+    public List<Texture2DArray> tex;
+
+    [SerializeField, Range(0, 3)]
+    public int AnimationIndex;
+
+    public bool RandomizeAnimation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        if (RandomizeAnimation)
+        {
+            SetAnimation(Random.Range(0, 4));
+        }
+        else
+        {
+            SetAnimation(AnimationIndex);
+        }
+    }
+
+    public void SetAnimation(int idx)
     {
         MaterialPropertyBlock properties = new();
         GetComponent<Renderer>().GetPropertyBlock(properties);
 
         // Set random values
+        properties.SetFloat("_Frames", frameCount[idx]);
         properties.SetFloat("_StartTime", Random.Range(0.0f, 4.0f));
+        properties.SetTexture("_Anim", tex[idx]);
 
         // Apply the property block to the renderer
         GetComponent<Renderer>().SetPropertyBlock(properties);
